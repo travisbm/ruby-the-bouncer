81f65bb6c476fb3e57ba2a46e795964508856242
Travis Montgomery <travisbm@gmail.com>
2015-08-25 18:32:27 -0500

    removed permissions array from both functions to dry them up

--- bouncer.rb
+++ bouncer.rb
@@ -1,22 +1,20 @@
-def check_age(age)
-  permissions = ["do nothing", "vote", "smoke", "drink", "rent a car"]
+permissions = ["do nothing", "vote", "smoke", "drink", "rent a car"]
 
+def check_age(age, array)
   if age < 18
-    puts "You can #{permissions[0]}!"
+    puts "You can #{array[0]}!"
   elsif age >= 18 && age < 21
-    puts "You can #{permissions[1]} and #{permissions[2]}!"
+    puts "You can #{array[1]} and #{array[2]}!"
   elsif age >= 21 && age < 25
-    puts "You can #{permissions[1]}, #{permissions[2]} and #{permissions[3]}!"
+    puts "You can #{array[1]}, #{array[2]} and #{array[3]}!"
   elsif age >= 25
-    puts "You can #{permissions[1]}, #{permissions[2]}, #{permissions[3]} and #{permissions[4]}!"
+    puts "You can #{array[1]}, #{array[2]}, #{array[3]} and #{array[4]}!"
   else
     exit
-  end
-end
+  end #end of if statement
+end #end check_age defenition
 
-def check_age_uk(age)
-  permissions = ["do nothing", "vote", "smoke", "drink", "rent a car"]
-
+def check_age_uk(age, array)
   if age < 18
     if age == 16 || age == 17
       print "Are your parents home (Y)es or (N)o > "
@@ -24,17 +22,17 @@ def check_age_uk(age)
       if ans.to_s == "Y"
         puts "You can drink with your mom, dork."
       else
-        puts "You can #{permissions[0]}!"
+        puts "You can #{array[0]}!"
       end #end of yes no if statement
     end #end of 16 or 17 if statement
   elsif age >= 18 && age <= 25
-    puts "You can #{permissions[1]}, #{permissions[2]} and #{permissions[3]}!"
+    puts "You can #{array[1]}, #{array[2]} and #{array[3]}!"
   elsif age >= 25
-    puts "You can #{permissions[1]}, #{permissions[2]}, #{permissions[3]} and #{permissions[4]}!"
+    puts "You can #{array[1]}, #{array[2]}, #{array[3]} and #{array[4]}!"
   else
     exit
-  end
-end
+  end #end of initial if statement
+end #end of check_age_defenition
 
 loop do
   print "What country are you from? Enter US or UK > "
@@ -46,9 +44,9 @@ loop do
   if age == "Q"
     exit
   elsif country == "US"
-    check_age(age.to_i)
+    check_age(age.to_i, permissions)
   elsif country == "UK"
-    check_age_uk(age.to_i)
+    check_age_uk(age.to_i, permissions)
   else
     puts "You are typing nonsense!"
   end #end of if statement
