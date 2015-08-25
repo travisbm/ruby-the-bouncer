dd12c53ec2f8d4393fe72c969754c427e55c4f23
Travis Montgomery <travisbm@gmail.com>
2015-08-25 15:13:06 -0500

    added check age function with first range check

--- bouncer.rb
+++ bouncer.rb
@@ -2,3 +2,15 @@ print "How old are you? > "
 age = gets.chomp.to_i
 
 puts age
+
+def check_age(age)
+  array = ["do nothing", "vote", "smoke", "drink", "rent a car"]
+
+  if age < 18
+    puts "You can #{array[0]}!"
+  else
+    exit
+  end
+end
+
+check_age(age)
